5fbb7440bac411b7fe2bf9aaf2c67ce4fa7264b7
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-07-25 03:59:59 -0700

    Completes lab: practices use of list methods and comprehensions

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -16,23 +16,48 @@ spicy_foods = [
     },
 ]
 
+
 def get_names(spicy_foods):
-    pass
+    names = list()
+    for food in spicy_foods:
+        names.append(food["name"])
+    return names
+
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
+
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        print(
+            f"{food['name']} ({food['cuisine']}) | Heat Level: {'🌶' * food['heat_level']}"
+        )
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"].lower() == cuisine.lower():
+            return food
+
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = list()
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            spiciest_foods.append(food)
+    print_spicy_foods(spiciest_foods)
+
 
 def get_average_heat_level(spicy_foods):
-    pass
+    heat_level_total = 0
+    counter = 0
+    for food in spicy_foods:
+        heat_level_total += food["heat_level"]
+        counter += 1
+    return int(heat_level_total / counter)
+
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
